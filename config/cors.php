8bb39dc3856f320a15dd262eaf3ca6e42b62b2d1
Tchablenan <108093174+Tchablenan@users.noreply.github.com>
2025-10-03 11:17:29 +0000

    ajout du nom de domain

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [  'http://localhost:5174','http://127.0.0.1:5174',],
+    'allowed_origins' => [  'http://localhost:5174','http://127.0.0.1:5174', 'https://aljannahjet.com','https://www.aljannahjet.com',],
 
     'allowed_origins_patterns' => [],
 
